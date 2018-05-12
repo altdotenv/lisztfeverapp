85335249c68b78f02f2305103cb64221d50c9fe8
jisuhan3201 <jisu.han3201@gmail.com>
2018-05-12 10:52:43 +0900

    Add DB settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,16 @@ ALLOWED_HOSTS = ["*"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
-# DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('RDS_DB_NAME'),
+        'USER': env('RDS_DB_USERNAME'),
+        'PASSWORD': env('RDS_DB_PASSWORD'),
+        'HOST': env('RDS_DB_HOSTNAME'),
+        'PORT': env('RDS_DB_PORT'),
+    }
+}
 DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
 DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
