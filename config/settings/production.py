8db0d4741dce9aeaf13dad9bab1e9d4be88a73df
jisuhan3201 <jisu.han3201@gmail.com>
2018-05-19 18:15:24 +0900

    dsafds

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,20 +12,20 @@ ALLOWED_HOSTS = ["*"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('RDS_DB_NAME'),
-        'USER': env('RDS_DB_USERNAME'),
-        'PASSWORD': env('RDS_DB_PASSWORD'),
-        'HOST': env('RDS_DB_HOSTNAME'),
-        'PORT': env('RDS_DB_PORT'),
-        'OPTIONS': {
-            'charset': 'utf8',
-            'use_unicode': True,
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': env('RDS_DB_NAME'),
+#         'USER': env('RDS_DB_USERNAME'),
+#         'PASSWORD': env('RDS_DB_PASSWORD'),
+#         'HOST': env('RDS_DB_HOSTNAME'),
+#         'PORT': env('RDS_DB_PORT'),
+#         'OPTIONS': {
+#             'charset': 'utf8',
+#             'use_unicode': True,
+#         },
+#     }
+# }
 DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
 DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
