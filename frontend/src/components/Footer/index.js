06540083f77f6b588221e7dfc6ea860a3398800d
jisuhan3201 <jisu.han3201@gmail.com>
2018-05-22 18:30:24 +0900

    Change AREHA CO.

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -5,7 +5,7 @@ import styles from "./styles.scss";
 const Footer = (props, context) => (
   <footer className={styles.footer}>
     <div className={styles.column}>
-      <span className={styles.copyright}>&copy; 2018 AREHA.co</span>
+      <span className={styles.copyright}>&copy; 2018 AREHA CO.</span>
     </div>
   </footer>
 )
