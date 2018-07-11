2c4e60eb87808c1bb0eff3b0d8035eb38ba3a3dc
jisuhan3201 <jisu.han3201@gmail.com>
2018-07-11 15:07:06 +0900

    fix production settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -176,57 +176,53 @@ AWS_PRELOAD_METADATA = True
 LOG_PATH = os.path.join('/var/log/', "web-actions/")
 LOGGING = {
     'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
+    'disable_existing_loggers': True,
     'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s '
-                      '%(process)d %(thread)d %(message)s'
-        },
         'standard': {
             'format': '%(asctime)s [%(levelname)s]- %(message)s'
         }
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
+        'django_error': {
+            'level': 'WARNING',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': LOG_PATH + 'django.log',
+            'formatter': 'standard'
         },
         'info': {
             'level': 'INFO',
             'class': 'logging.handlers.RotatingFileHandler',
             'filename': LOG_PATH + 'info.log',
-            'maxBytes': 50000,
             'formatter': 'standard'
         },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        }
     },
     'loggers': {
         'info': {
-            'handlers': ['info'],
-            'level': 'INFO',
+            'handlers': ['info', "console"],
+            'level': 'DEBUG',
             'propagate': True
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
         },
         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True
+            'handlers': ['django_error', 'console'],
+            'level': 'DEBUG',
+            'propagate': True,
         },
-        'django.security.DisallowedHost': {
+        'django.db.backends': {
+            'handlers': ['console'],
             'level': 'ERROR',
-            'handlers': ['console', 'mail_admins'],
-            'propagate': True
+            'propagate': True,
         }
-    }
+    },
 }
 # Your stuff...
 # ------------------------------------------------------------------------------
